6b4aa21092e4866f68c5bd2187363ca2c9403c2b
Eduardo Henrique <105989448+duduhenryy@users.noreply.github.com>
2025-03-20 14:34:44 -0300

    Update 1_upar_imagem.py

--- tarefa1/codigos/1_upar_imagem.py
+++ tarefa1/codigos/1_upar_imagem.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import cv2
 
-img = cv2.imread("tarefa1/amor.jpeg")
+img = cv2.imread("tarefa1/imgs/amor.jpeg")
 
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # converte o bgr para o rgb
 
