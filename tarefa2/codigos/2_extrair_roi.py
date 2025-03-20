4108fc91e2760da317a159c74396d788ac6991e4
Eduardo Henrique <105989448+duduhenryy@users.noreply.github.com>
2025-03-20 14:46:24 -0300

    refactor commit

--- tarefa2/codigos/2_extrair_roi.py
+++ tarefa2/codigos/2_extrair_roi.py
@@ -6,7 +6,7 @@ def crop(image, x, y, width, height):
     # o mesmo ocorre com o x
     return corte
 
-img = cv2.imread('tarefa2/amor.jpeg')
+img = cv2.imread('tarefa2/sources/amor.jpeg')
 
 x_relogio = 879
 y_relogio = 493
@@ -15,6 +15,6 @@ altura_relogio = 66
 
 clockImg = crop(img, x_relogio, y_relogio, largura_relogio, altura_relogio)
 
-output_path = 'tarefa2/clockImg.jpeg'
+output_path = 'tarefa2/sources/clockImg.jpeg'
 cv2.imwrite(output_path, clockImg)
 print(f"Imagem salva em: {output_path}")
